543add68c17f8ae932005ee4058bca16dd916426
張睿Zinzan <www.zinzan.msn@hotmail.com>
2022-06-03 21:07:37 +0800

    加入專案檔案。

---
+++ Homwwork_20220529/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Homwwork_20220529.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
